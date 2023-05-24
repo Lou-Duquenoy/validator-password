072236825742e2cfcd7de5e034c6b082cf671c47
Lou Duquenoy <72021807+Lou-Duquenoy@users.noreply.github.com>
2023-05-24 16:59:40 +0200

    final-commit-fix-add-folders-missing

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ import passwordValidator from "./passwordValidator";
 const passwordInput = document.getElementById("password");
 const errorMessage = document.querySelector(".error-message");
 const form = document.getElementById("form");
-console.log(form);
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const password = passwordInput.value;
   const errors = passwordValidator(password);
-  
+
   if (errors.length > 0) {
     errorMessage.textContent = errors.join(" ");
   } else {
